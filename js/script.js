5965bcbf1d1f1cba5265400b31bed059c4e7c42c
afsana04 <129384941+afsana04@users.noreply.github.com>
2023-09-02 20:29:50 +0600

    CIT-Final-Project

---
+++ js/script.js
@@ -0,0 +1,112 @@
+
+//=======OFFER ITEM SLIDER======
+// $('.offer_item_slider').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 4000,
+//     dots: false,
+//     arrows: true,
+//     nextArrow: '<i class="far fa-long-arrow-right nextArrow"></i>',
+//     prevArrow: '<i class="far fa-long-arrow-left prevArrow"></i>',
+//     responsive: [
+//         {
+//             breakpoint: 1400,
+//             settings: {
+//                 slidesToShow: 2,
+//             }
+//         },
+//         {
+//             breakpoint: 1200,
+//             settings: {
+//                 slidesToShow: 2,
+//             }
+//         },
+//         {
+//             breakpoint: 992,
+//             settings: {
+//                 slidesToShow: 2,
+//             }
+//         },
+//         {
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 1,
+//             }
+//         },
+//         {
+//             breakpoint: 576,
+//             settings: {
+//                 arrows: false,
+//                 slidesToShow: 1,
+//             }
+//         }
+//     ]
+// });
+
+$('.offer_item_wrap').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: ".left_btn",
+    nextArrow: ".right_btn",
+  });
+
+  // ========FOOD MENU MIXIT UP===========
+  var mixer = mixitup('.food_item_wrap', {
+    selectors: {
+        target: '.food_item'
+    },
+    animation: {
+        duration: 300
+    }
+  });
+  // =========Chefs==========
+  $('.team_wrap').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: ".left_btn",
+    nextArrow: ".right_btn",
+  });
+  // ========Order==========
+  $('.order_item_wrap').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+
+  });
+  // =========Testimonial========
+  $('.testimonial_wrap').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: ".left_btn",
+    nextArrow: ".right_btn",
+  });
+  // =========Counter_up============
+  $(function(){
+    console.log("ready");
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000  
+    });
+})
+// ==========BLOG==========
+$('.blog_wrap').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: true,
+  prevArrow: ".left_btn",
+  nextArrow: ".right_btn",
+});
